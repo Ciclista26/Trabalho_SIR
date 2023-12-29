ba0577b77adef31a75ff9fd0f959c610774a66df
Rui Alves <ruijorge02012001@gmail.com>
2023-12-29 16:58:19 +0000

    erro

--- infra/middlewares/middleware-administrator.php
+++ infra/middlewares/middleware-administrator.php
@@ -3,11 +3,6 @@
 @require_once __DIR__ . '/../../helpers/session.php';
 
 if (!administrator()) {
-    $signin_url = 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/pages/public/signin.php';
-    header('Location: ' . $signin_url);
-    exit(); 
+    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/';
+    header('Location: ' . $home_url);
 }
-
-$original_page = isset($_SESSION['original_page']) ? $_SESSION['original_page'] : 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/pages/admin/default_admin_page.php';
-header('Location: ' . $original_page);
-exit();
