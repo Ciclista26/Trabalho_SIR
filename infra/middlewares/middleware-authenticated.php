4f4d14ba5a2b7eecdfb2a26a7aa0beac1406bc88
Rui Alves <ruijorge02012001@gmail.com>
2023-12-29 16:33:01 +0000

    redirecionar para a pagina original

--- infra/middlewares/middleware-authenticated.php
+++ infra/middlewares/middleware-authenticated.php
@@ -3,6 +3,11 @@
 require_once __DIR__ . '/../../helpers/session.php';
 
 if (!isAuthenticated()) {
-    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/';
-    header('Location: ' . $home_url);
+    $signin_url = 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/pages/public/signin.php';
+    header('Location: ' . $signin_url);
+    exit();
 }
+
+$original_page = isset($_SESSION['original_page']) ? $_SESSION['original_page'] : 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/pages/public/default_page.php';
+header('Location: ' . $original_page);
+exit();
--- infra/middlewares/middleware-user.php
+++ infra/middlewares/middleware-user.php
@@ -3,11 +3,24 @@
 session_start();
 
 if (!isset($_SESSION['id'])) {
+  
   if (isset($_COOKIE['id']) && isset($_COOKIE['name'])) {
     $_SESSION['id'] = $_COOKIE['id'];
     $_SESSION['name'] = $_COOKIE['name'];
   } else {
-    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/';
+    $_SESSION['redirect_url'] = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/Trabalho_SIR/pages/public/signin.php';
     header('Location: ' . $home_url);
+    exit();
   }
 }
+
+if (isset($_SESSION['id']) && isset($_SESSION['redirect_url'])) {
+  $redirect_url = $_SESSION['redirect_url'];
+  unset($_SESSION['redirect_url']);
+  header('Location: ' . $redirect_url);
+  exit();
+}
+
+?>
