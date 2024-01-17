46bc959d1dec18b6e68e738d087a14fd43b645be
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-01-17 14:11:48 +0000

    footer

--- pages/secure/user/resp_votacao.php
+++ pages/secure/user/resp_votacao.php
@@ -128,6 +128,12 @@ $userPhotoPath =  "../assets/images/uploads/" . $user['foto'];
             ?>
         </div>
     </div>
-    <?php
-    include_once __DIR__ . '../../../../templates/footer.php';
-    ?>
+    <!-- <?php include_once __DIR__ . '../../../templates/footer.php'; ?> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+  <script src="../js/script_user.js"></script>
+  <script src="../js/search.js"></script>
+</body>
+
+</html>
